23f1a33dfba0332761ddaa24eeb1729561072b6f
dsubnet0 <dsubnet0@gmail.com>
2020-01-26 15:31:42 -0500

    Refactor to function-driven

--- PullWebpageData.py
+++ PullWebpageData.py
@@ -3,29 +3,36 @@ import xmltodict
 
 API_KEY = 'gslnNIUU1HVFZN4kQig'
 API_SECRET = 'c2Q3V74CjGsaqcKzJ86ueTCf7Zzj1OIJ0kKGw4jf0'
-def get_toread_titles():
-    pass
+
+
+def get_toread_titles(user_id):
+    url = 'https://www.goodreads.com/review/list.xml?v=2&key={key}&id={id}&shelf=to-read'.format(key=API_KEY,
+                                                                                                 id=user_id)
+    try:
+        r = requests.get(url)
+    except e:
+        print(e)
+    if not r:
+        print('Some problem with the request')
+        return -1
+
+    shelf_contents = xmltodict.parse(r.text)
+    title_list = []
+    for entry in shelf_contents['GoodreadsResponse']['reviews']['review']:
+        entry_items = entry['book']
+        print(entry_items['title'])
+    return 0
 
 
 USER_ID = '3696598'
-#url = 'https://www.goodreads.com/review/list/3696598-doug?shelf=to-read'
-url = 'https://www.goodreads.com/review/list.xml?v=2&key={key}&id={id}&shelf=to-read'.format(key=api_key,id=user_id)
-try:
-    r = requests.get(url)
-except e:
-    print(e)
 
-if r:
-    print('Successful request')
-else:
-    print('Some problem with the request')
+if __name__ == '__main__':
+    get_toread_titles(USER_ID)
 
 #print(r.text)
 
-shelf_contents = xmltodict.parse(r.text)
 
-for book in shelf_contents['GoodreadsResponse']['reviews']['review']:
-    for key,value in book['book'].items():
-        if key == 'title':
-            print('Title:',value)
+#    for key,value in book['book'].items():
+#        if key == 'title':
+#            print('Title:',value)
 

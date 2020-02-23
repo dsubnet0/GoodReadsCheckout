855d45a739a7ad90bf5bcbcb2b42ce351a1fa38a
dsubnet0 <dsubnet0@gmail.com>
2020-02-23 16:47:31 -0500

    Final tweaks

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ from QueryWebpage import query_southbury_library
 USER_ID = '3696598'
 
 titles_considered = 0
-for t in get_toread_titles(USER_ID):
-    print("Searching for {}...".format(t))
-    for r in query_southbury_library(t):
-        if len(t):
-            print("|".join(r))
-    titles_considered += 1
+titles_hit = 0
+page = 0
+while titles_hit < 50:
+    page += 1
+    for t in get_toread_titles(USER_ID, page):
+#        print("Searching for {}...".format(t))
+        for r in query_southbury_library(t):
+            titles_hit += 1
+            if len(r)>0:
+                print("|".join(r))
+        titles_considered += 1
 print("titles searched: "+str(titles_considered))
+print("results returned: "+str(titles_hit))
